4d58669abf206fa62d55c59e6b248f627da2a8d1
GuanYanqi <admin@guanyanqi.com>
2026-05-30 10:52:50 +0800

    改成老师写的代码

--- process/import_/agent/nodes/node_entry.py
+++ process/import_/agent/nodes/node_entry.py
@@ -3,9 +3,32 @@ from app.shared.utils.task_utils import add_done_task, add_running_task
 from app.process.import_.agent.state import ImportGraphState
 from app.rag.import_.entry_service import resolve_input_file
 
+"""
+   @node_log("node_entry")  -> 方法进入 和 方法执行完毕 以及异常的日志 
+   节点方法(state : ImportGraphState)  
+       打印开始的日志
+       1. add_running_task(state["task_id"], "node_entry")
+       2. state = resolve_input_file(state) 调用节点业务函数 [节点 + 业务的关联]
+       3. add_done_task(state["task_id"], "node_entry")
+       开营结束的日志
+       return state 
+
+   为啥add_running_task / add_done_task: 记录节点的完成状态, 存储到对应task_id的列表中 (此时:还是英文)
+   为啥要传入task_id :  因为我们有很多客户端,为每个客户端存储一个对应的列表, 最终存储到dict [task_id, [] ]
+   节点名称是固定的么: 是的,因为后续 get的时候使用列表推到式 将英文 转成对应的中文,定义的时候 key 就是节点!
+
+   @node_log("节点名")  -> 方法进入 和 方法执行完毕 以及异常的日志 
+   节点方法(state : ImportGraphState)  
+       打印开始的日志
+       1. add_running_task(state["task_id"], "node_entry")
+       2. state = resolve_input_file(state) 调用节点业务函数 [节点 + 业务的关联]
+       3. add_done_task(state["task_id"], "node_entry")
+       开营结束的日志
+       return state 
+"""
 
 
-@node_log("node_entry") # 打印日志装饰器 方法进入和方法完毕 和异常的日志
+@node_log("node_entry")
 def node_entry(state: ImportGraphState) -> ImportGraphState:
     """
     节点: 入口节点 (node_entry)
